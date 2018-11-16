e36d36af50c9df31742daa71ca2aafcc3bf42038
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2018-11-16 15:37:50 +0000

    Added a door for the second level

---
+++ Assets/Scenes/End.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Extra using statement to allow us to use the scene management functions
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour {
+
+    //Designer Variables
+    public string sceneToLoad;
+
+    // Unity cals this function automatically
+    // when our spikes touch any other object
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the thing that we collded with 
+        // is the player (aka has a Player script)
+        Player playerScript = collision.collider.GetComponent<Player>();
+
+        // Only do somehting if the thing we ran into
+        // was in fact the player
+        // aka playerscript is not null
+
+        if (playerScript != null)
+        {
+
+            // We DID hit the player!
+
+            // load next level
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
