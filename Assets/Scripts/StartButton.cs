d20683b7149eb4ced9ae35025ea2b067a87682f4
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2018-11-30 14:28:24 +0000

    Added title screen and game over screen

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+//This alows us to use the scene loading function
+
+public class StartButton : MonoBehaviour {
+
+    //This will be called by the button component
+    // When the button is clicked
+    public void StartGame()
+    {
+        //Reset the score
+        PlayerPrefs.DeleteKey("score");
+
+        SceneManager.LoadScene("Level 1");
+    }
+
+
+}
